427f900b585a9b206dc133998d2454ea2653164b
Erick <erick.rodriguez@iteso.mx>
2025-05-08 00:00:23 -0600

    Cambios Toy story

--- FRONTEND/controllers/Users_controller.js
+++ FRONTEND/controllers/Users_controller.js
@@ -20,9 +20,7 @@ function init(){
             userrank = document.getElementById('userrank'),
             useraciertos = document.getElementById('useraciertos');
 
-        getUsers().then(Usuarios => {
-            //Obtenemos al usuario
-            Usuarios.find( ele => user_account.id = ele.id);
+            //Colocamos los datos del usuario
             username.innerText = user_account.name;
             usermsg.innerText = user_account.message;
             usercatfav.innerText = user_account.catfav;
@@ -36,7 +34,66 @@ function init(){
                 if( categorias[i] === user_account.catfav ) banner.style.backgroundColor = purple;
             }
 
-        });
+            //Obtenemos las preguntas que ya respondio
+            getHistorial().then(questions =>{
+                questions.forEach(question => {
+                    //creamos el formato de cada pregunta
+                    let rowQuestions = document.getElementById('rowQuestions');
+                    if(questions.length > 0){
+                        //Si el mensaje existe, lo quitamos por que ya estamos en una etiqueta que SI tiene tareas
+                        let prevmsg = document.getElementById('msg');
+                        if( prevmsg != undefined ) prevmsg.remove();
+
+                        let row = document.createElement('div');
+                        row.classList.add('col-12', 'col-sm-6', 'col-lg-4');
+                        let quest = document.createElement('div');
+                        quest.classList.add('question', 'p-3');
+                        let flex = document.createElement('div');
+                        flex.classList.add('d-flex');
+
+                        let preg = document.createElement('div');
+                        preg.classList.add('me-3');
+                        let b1 = document.createElement('b');
+                        b1.innerText = question.descripcion;
+                        let p = document.createElement('p');
+                        p.innerText = question.answer;
+                        preg.append(b1,p);
+
+                        let i = document.createElement('i');
+                        i.classList.add('bi','fs-1');
+                        if(question.status != true){
+                            quest.classList.add('wrong');
+                            i.classList.add('bi-check-circle');
+                        } 
+                        else{
+                            quest.classList.add('right');
+                            i.classList.add('bi-x-circle');
+                        } 
+                        flex.append(preg,i);
+
+                        let gen = document.createElement('div');
+                        gen.classList.add('me-2');
+                        let b2 = document.createElement('b');
+                        b2.innerText = 'Categoria: ';
+                        let span = document.createElement('span');
+                        span.innerText = question.categoria;
+                        gen.append(b2,span);
+
+                        quest.append(flex,gen);
+                        row.append(quest);
+                        //Pegamos todo el contenido de la pregunta dentro del contenedor
+                        rowQuestions.append(row);
+                    }else{
+                        let mensaje = document.createElement('p');
+                        mensaje.id = 'msg';
+                        mensaje.style.color = "white";
+                        mensaje.style.textAlign = 'center';
+                        mensaje.innerText = '\nJuega una partida para empezar tu historial!.';
+                        //Si el mensaje no existe todavia, lo agregamos evitando repeticiones
+                        if( document.getElementById('msg') == undefined ) rowQuestions.append(mensaje);
+                    }
+                });
+            }).catch(err => {console.log('Error al obtener el historial del usuario: ' + err);});
     }
 
     //Si estas en el EditProfile
@@ -51,7 +108,7 @@ function init(){
         txtmsg.innerText = user_account.message;
         cbcategoria.value = user_account.catfav;
         txtpass.innerText = user_account.password;
-        
+
     }
 
 }
@@ -90,6 +147,29 @@ function logout(){
     window.location.href = local_url+'login.html';
 }
 
+async function getUser(){
+    let user = await fetch('/users/' + sessionStorage.user.id, {
+        method: 'GET'
+    }).then(async (response) => {
+        if(!response.ok) alert(await response.text());
+        return await response.json();
+    }).catch(err =>{
+        console.error("Fallo al obtener al usuario: " + err)
+    });
+    return user;
+}
+
+async function getHistorial(){
+    let his = await fetch('/histories/' + sessionStorage.user.id, {
+        method: 'GET'
+    }).then(async (response) => {
+        if(!response.ok) alert(await response.text());
+        return await response.json();
+    }).catch(err =>{
+        console.error("Fallo al obtener el historial del usuario: " + err)
+    });
+    return his;
+}
 //--------------------------------------------------------------------
 //--------------------------------------------------------------------
 init();
