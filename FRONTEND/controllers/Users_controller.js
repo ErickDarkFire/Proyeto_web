bfdfbc04ae21e86122fa7c02de47a429c97334d2
Erick <erick.rodriguez@iteso.mx>
2025-05-08 00:06:58 -0600

    control de acceso

--- FRONTEND/controllers/Users_controller.js
+++ FRONTEND/controllers/Users_controller.js
@@ -7,108 +7,121 @@ function init(){
     
     //Si estas en el leaderboard
     if(window.location.href == local_url+"leaderboard.html"){
+        if( user_account != undefined ){
+            alert("Favor de iniciar sesión para ver el leaderboard");
+            window.location.href = local_url + "home.html";
+        }
         console.log("xd");
     }
 
     //Si estas en el profile
     if(window.location.href == local_url+"Profile.html"){
-        //obtenemos los componentes donde colocaremos la informacion
-        let username = document.getElementById('username'),
-            usermsg = document.getElementById('usermsg'),
-            usercatfav = document.getElementById('usercatfav'),
-            userpts = document.getElementById('userpts'),
-            userrank = document.getElementById('userrank'),
-            useraciertos = document.getElementById('useraciertos');
+        if( user_account != undefined ){
+            alert("Favor de iniciar sesión para acceder a tu perfil");
+            window.location.href = local_url + "home.html";
+        }else{
+            //obtenemos los componentes donde colocaremos la informacion
+            let username = document.getElementById('username'),
+                usermsg = document.getElementById('usermsg'),
+                usercatfav = document.getElementById('usercatfav'),
+                userpts = document.getElementById('userpts'),
+                userrank = document.getElementById('userrank'),
+                useraciertos = document.getElementById('useraciertos');
 
-            //Colocamos los datos del usuario
-            username.innerText = user_account.name;
-            usermsg.innerText = user_account.message;
-            usercatfav.innerText = user_account.catfav;
-            userpts.innerText = user_account.points;
-            userrank.innerText = user_account.rank;
-            useraciertos.innerText = user_account.aciertos;
-            
-            //dependiendo la categoria favorita, cambiamos el color del banner
-            let banner = document.getElementById('banner');
-            for (let i = 0; i < categorias.length; i++) {
-                if( categorias[i] === user_account.catfav ) banner.style.backgroundColor = purple;
-            }
+                //Colocamos los datos del usuario
+                username.innerText = user_account.name;
+                usermsg.innerText = user_account.message;
+                usercatfav.innerText = user_account.catfav;
+                userpts.innerText = user_account.points;
+                userrank.innerText = user_account.rank;
+                useraciertos.innerText = user_account.aciertos;
+                
+                //dependiendo la categoria favorita, cambiamos el color del banner
+                let banner = document.getElementById('banner');
+                for (let i = 0; i < categorias.length; i++) {
+                    if( categorias[i] === user_account.catfav ) banner.style.backgroundColor = purple;
+                }
 
-            //Obtenemos las preguntas que ya respondio
-            getHistorial().then(questions =>{
-                questions.forEach(question => {
-                    //creamos el formato de cada pregunta
-                    let rowQuestions = document.getElementById('rowQuestions');
-                    if(questions.length > 0){
-                        //Si el mensaje existe, lo quitamos por que ya estamos en una etiqueta que SI tiene tareas
-                        let prevmsg = document.getElementById('msg');
-                        if( prevmsg != undefined ) prevmsg.remove();
+                //Obtenemos las preguntas que ya respondio
+                getHistorial().then(questions =>{
+                    questions.forEach(question => {
+                        //creamos el formato de cada pregunta
+                        let rowQuestions = document.getElementById('rowQuestions');
+                        if(questions.length > 0){
+                            //Si el mensaje existe, lo quitamos por que ya estamos en una etiqueta que SI tiene tareas
+                            let prevmsg = document.getElementById('msg');
+                            if( prevmsg != undefined ) prevmsg.remove();
 
-                        let row = document.createElement('div');
-                        row.classList.add('col-12', 'col-sm-6', 'col-lg-4');
-                        let quest = document.createElement('div');
-                        quest.classList.add('question', 'p-3');
-                        let flex = document.createElement('div');
-                        flex.classList.add('d-flex');
+                            let row = document.createElement('div');
+                            row.classList.add('col-12', 'col-sm-6', 'col-lg-4');
+                            let quest = document.createElement('div');
+                            quest.classList.add('question', 'p-3');
+                            let flex = document.createElement('div');
+                            flex.classList.add('d-flex');
 
-                        let preg = document.createElement('div');
-                        preg.classList.add('me-3');
-                        let b1 = document.createElement('b');
-                        b1.innerText = question.descripcion;
-                        let p = document.createElement('p');
-                        p.innerText = question.answer;
-                        preg.append(b1,p);
+                            let preg = document.createElement('div');
+                            preg.classList.add('me-3');
+                            let b1 = document.createElement('b');
+                            b1.innerText = question.descripcion;
+                            let p = document.createElement('p');
+                            p.innerText = question.answer;
+                            preg.append(b1,p);
 
-                        let i = document.createElement('i');
-                        i.classList.add('bi','fs-1');
-                        if(question.status != true){
-                            quest.classList.add('wrong');
-                            i.classList.add('bi-check-circle');
-                        } 
-                        else{
-                            quest.classList.add('right');
-                            i.classList.add('bi-x-circle');
-                        } 
-                        flex.append(preg,i);
+                            let i = document.createElement('i');
+                            i.classList.add('bi','fs-1');
+                            if(question.status != true){
+                                quest.classList.add('wrong');
+                                i.classList.add('bi-check-circle');
+                            } 
+                            else{
+                                quest.classList.add('right');
+                                i.classList.add('bi-x-circle');
+                            } 
+                            flex.append(preg,i);
 
-                        let gen = document.createElement('div');
-                        gen.classList.add('me-2');
-                        let b2 = document.createElement('b');
-                        b2.innerText = 'Categoria: ';
-                        let span = document.createElement('span');
-                        span.innerText = question.categoria;
-                        gen.append(b2,span);
+                            let gen = document.createElement('div');
+                            gen.classList.add('me-2');
+                            let b2 = document.createElement('b');
+                            b2.innerText = 'Categoria: ';
+                            let span = document.createElement('span');
+                            span.innerText = question.categoria;
+                            gen.append(b2,span);
 
-                        quest.append(flex,gen);
-                        row.append(quest);
-                        //Pegamos todo el contenido de la pregunta dentro del contenedor
-                        rowQuestions.append(row);
-                    }else{
-                        let mensaje = document.createElement('p');
-                        mensaje.id = 'msg';
-                        mensaje.style.color = "white";
-                        mensaje.style.textAlign = 'center';
-                        mensaje.innerText = '\nJuega una partida para empezar tu historial!.';
-                        //Si el mensaje no existe todavia, lo agregamos evitando repeticiones
-                        if( document.getElementById('msg') == undefined ) rowQuestions.append(mensaje);
-                    }
-                });
-            }).catch(err => {console.log('Error al obtener el historial del usuario: ' + err);});
+                            quest.append(flex,gen);
+                            row.append(quest);
+                            //Pegamos todo el contenido de la pregunta dentro del contenedor
+                            rowQuestions.append(row);
+                        }else{
+                            let mensaje = document.createElement('p');
+                            mensaje.id = 'msg';
+                            mensaje.style.color = "white";
+                            mensaje.style.textAlign = 'center';
+                            mensaje.innerText = '\nJuega una partida para empezar tu historial!.';
+                            //Si el mensaje no existe todavia, lo agregamos evitando repeticiones
+                            if( document.getElementById('msg') == undefined ) rowQuestions.append(mensaje);
+                        }
+                    });
+                }).catch(err => {console.log('Error al obtener el historial del usuario: ' + err);});
+        }
     }
 
     //Si estas en el EditProfile
     if(window.location.href == local_url+"EditProfile.html"){
-        let txtName = document.getElementById('txtName'),
-            cbcategoria = document.getElementById('cbcategoria'),
-            txtmsg = document.getElementById('txtmsg'),
-            txtpass = document.getElementById('txtpass'),
-            txtConfpass = document.getElementById('txtConfpass');
-        
-        txtName.innerText = user_account.name;
-        txtmsg.innerText = user_account.message;
-        cbcategoria.value = user_account.catfav;
-        txtpass.innerText = user_account.password;
-
+        if( user_account != undefined ){
+            alert("Favor de iniciar sesión para acceder a tu perfil");
+            window.location.href = local_url + "home.html";
+        }else{
+            let txtName = document.getElementById('txtName'),
+                cbcategoria = document.getElementById('cbcategoria'),
+                txtmsg = document.getElementById('txtmsg'),
+                txtpass = document.getElementById('txtpass'),
+                txtConfpass = document.getElementById('txtConfpass');
+            
+            txtName.innerText = user_account.name;
+            txtmsg.innerText = user_account.message;
+            cbcategoria.value = user_account.catfav;
+            txtpass.innerText = user_account.password;
+        }
     }
 
 }
@@ -170,6 +183,7 @@ async function getHistorial(){
     });
     return his;
 }
+
 //--------------------------------------------------------------------
 //--------------------------------------------------------------------
 init();
